cf632e810c1a227939d2cb108e7b72c5887faac5
Jason Nelson <jason@carbonmade.com>
2021-09-24 22:22:19 -0700

    [Tests] Target net6.0

--- src/StatHat.Tests/EZRequestTests.cs
+++ src/StatHat.Tests/EZRequestTests.cs
@@ -5,43 +5,42 @@ using StatHat.Models;
 
 using Xunit;
 
-namespace StatHat.Tests
+namespace StatHat.Tests;
+
+public class EZRequestTests
 {
-    public class EZRequestTests
+    /*
+    [Fact]
+    public void SerializeEZStat()
     {
-        /*
-        [Fact]
-        public void SerializeEZStat()
-        {
-            var a = new EZRequest("key", new[] { new EZStat("counter", 1) });
+        var a = new EZRequest("key", new[] { new EZStat("counter", 1) });
 
-            var json = JsonObject.FromObject(a);
+        var json = JsonObject.FromObject(a);
 
-            Assert.Equal(@"{""ezkey"":""key"",""data"":[{""stat"":""counter"",""count"":1}]}", json.ToString(false));
-            Assert.Equal(@"{""ezkey"":""key"",""data"":[{""stat"":""counter"",""count"":1}]}", a.ToString());
-        }
-        */
+        Assert.Equal(@"{""ezkey"":""key"",""data"":[{""stat"":""counter"",""count"":1}]}", json.ToString(false));
+        Assert.Equal(@"{""ezkey"":""key"",""data"":[{""stat"":""counter"",""count"":1}]}", a.ToString());
+    }
+    */
 
-        [Fact]
-        public void SerializeMultipleEZTests()
-        {
-            var a = new EZRequest("a", new[] { new EZStat("counter", 0, 1), new EZStat("counter", 0, 2) });
+    [Fact]
+    public void SerializeMultipleEZTests()
+    {
+        var a = new EZRequest("a", new[] { new EZStat("counter", 0, 1), new EZStat("counter", 0, 2) });
 
-            Assert.Equal(@"{""ezkey"":""a"",""data"":[{""stat"":""counter"",""value"":1},{""stat"":""counter"",""value"":2}]}", a.ToString());
-        }
+        Assert.Equal(@"{""ezkey"":""a"",""data"":[{""stat"":""counter"",""value"":1},{""stat"":""counter"",""value"":2}]}", a.ToString());
+    }
 
-        [Fact]
-        public void SerializeEnumerable()
-        {
-            var a = new EZRequest("a", Stats().ToArray());
+    [Fact]
+    public void SerializeEnumerable()
+    {
+        var a = new EZRequest("a", Stats().ToArray());
 
-            Assert.Equal(@"{""ezkey"":""a"",""data"":[{""stat"":""counter"",""count"":1},{""stat"":""counter"",""count"":2}]}", a.ToString());
-        }
-        
-        private static IEnumerable<EZStat> Stats()
-        {
-            yield return new EZStat("counter", 1);
-            yield return new EZStat("counter", 2);
-        }
+        Assert.Equal(@"{""ezkey"":""a"",""data"":[{""stat"":""counter"",""count"":1},{""stat"":""counter"",""count"":2}]}", a.ToString());
+    }
+
+    private static IEnumerable<EZStat> Stats()
+    {
+        yield return new EZStat("counter", 1);
+        yield return new EZStat("counter", 2);
     }
 }
