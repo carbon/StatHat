490c406802e49cbab574e7a4679a7dfd20707ffd
Jason Nelson <jason@carbonmade.com>
2023-04-19 21:06:39 -0700

    Improve test coverage

--- src/StatHat.Tests/EZRequestTests.cs
+++ src/StatHat.Tests/EZRequestTests.cs
@@ -1,3 +1,6 @@
+using System.Buffers;
+using System.Text;
+
 using StatHat.Models;
 
 using Xunit;
@@ -34,6 +37,20 @@ public class EZRequestTests
         var a = new EZRequest("a", new[] { new EZStat("counter", 0, 1), new EZStat("counter", 0, 2) });
 
         Assert.Equal("""{"ezkey":"a","data":[{"stat":"counter","value":1},{"stat":"counter","value":2}]}"""u8.ToArray(), a.SerializeToUtf8Bytes());
+    }
+
+
+    [Fact]
+    public void SerializeUtf8BytesLoad()
+    {
+        var b = """{"ezkey":"thekey","data":[{"stat":"counter","value":1},{"stat":"counter","value":2}]}"""u8.ToArray();
+
+        var request = new EZRequest("thekey", new[] { new EZStat("counter", 0, 1), new EZStat("counter", 0, 2) });
+
+        for (int i = 0; i < 1_000_000; i++)
+        {
+            Assert.True(b.SequenceEqual(request.SerializeToUtf8Bytes()));
+        }
     }
 
     [Fact]
